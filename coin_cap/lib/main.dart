3d37445e75c274dd73399b264c6bac51f5e901c1
KevinChiputra <kevin.chiputra@binus.ac.id>
2025-02-15 22:05:59 +0700

    feat: get API_URL, get function, use GetIt and  dio, create AppConfig class

--- coin_cap/lib/main.dart
+++ coin_cap/lib/main.dart
@@ -1,8 +1,35 @@
-import 'package:coin_cap/pages/home_page.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
+import 'package:coin_cap/models/app_config.dart';
+import 'package:coin_cap/pages/home_page.dart';
+import 'package:coin_cap/services/http_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
+  registerHTTPService();
   runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString('assets/config/main.json');
+
+  Map _configData = jsonDecode(_configContent);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(
+      COIN_API_BASE_URL: _configData['COIN_API_BASE_URL'],
+    ),
+  );
+}
+
+void registerHTTPService() {
+  GetIt.instance.registerSingleton<HTTPService>(
+    HTTPService(),
+  );
 }
 
 class MyApp extends StatelessWidget {
