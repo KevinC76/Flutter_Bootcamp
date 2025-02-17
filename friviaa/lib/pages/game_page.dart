c9be0d09ba5ed892073a4f041ccf611ef070e81b
KevinChiputra <kevin.chiputra@binus.ac.id>
2025-02-17 14:28:05 +0700

    feat: create provider file, use provider, and create button

--- friviaa/lib/pages/game_page.dart
+++ friviaa/lib/pages/game_page.dart
@@ -1,24 +1,34 @@
 import "package:flutter/material.dart";
+import "package:friviaa/providers/game_page_provider.dart";
+import "package:provider/provider.dart";
 
 class GamePage extends StatelessWidget {
   double? _deviceHeight, _deviceWidth;
+
+  GamePageProvider? _pageProvider;
 
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
-    return _buildUI();
+    return ChangeNotifierProvider(
+      create: (_context) => GamePageProvider(context: context),
+      child: _buildUI(),
+    );
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: _deviceHeight! * 0.05),
-          child: _gameUI(),
+    return Builder(builder: (_context) {
+      _pageProvider = _context.watch<GamePageProvider>();
+      return Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: _deviceHeight! * 0.05),
+            child: _gameUI(),
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 
   Widget _gameUI() {
@@ -28,6 +38,15 @@ class GamePage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         _questionTextWidget(),
+        Column(
+          children: [
+            _trueButton(),
+            SizedBox(
+              height: _deviceHeight! * 0.01,
+            ),
+            _falseButton(),
+          ],
+        ),
       ],
     );
   }
@@ -39,6 +58,38 @@ class GamePage extends StatelessWidget {
         color: Colors.white,
         fontSize: 25,
         fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _trueButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.green,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        "True",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+
+  Widget _falseButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.red,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        "False",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
       ),
     );
   }
