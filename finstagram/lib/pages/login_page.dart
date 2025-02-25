e4bcf1f8200863e071b14d18bc9e864af728f5aa
KevinChiputra <kevin.chiputra@binus.ac.id>
2025-02-25 18:14:41 +0700

    feat: finis login page and create register page

---
+++ finstagram/lib/pages/login_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  double? _deviceHeight, _deviceWidth;
+
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
+  String? _email;
+  String? _password;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth! * 0.05),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _titleWidget(),
+                _loginForm(),
+                _loginButton(),
+                _registerPageLink(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return const Text(
+      'Fintagram',
+      style: TextStyle(
+          color: Colors.black, fontWeight: FontWeight.w600, fontSize: 25),
+    );
+  }
+
+  Widget _loginForm() {
+    return Container(
+      height: _deviceHeight! * 0.20,
+      child: Form(
+        key: _loginFormKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _emailTextField(),
+            _PasswordTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(hintText: 'Email...'),
+      onSaved: (_value) {
+        setState(() {
+          _email = _value;
+        });
+      },
+      validator: (_value) {
+        bool _result = _value!.contains(
+          RegExp(
+              r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"),
+        );
+        return _result ? null : "Please enter a valid email";
+      },
+    );
+  }
+
+  Widget _PasswordTextField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(hintText: 'Password...'),
+      onSaved: (_value) {
+        setState(() {
+          _password = _value;
+        });
+      },
+      validator: (_value) => _value!.length > 6
+          ? null
+          : "Please enter a password that greater than 6 character",
+    );
+  }
+
+  Widget _loginButton() {
+    return MaterialButton(
+      onPressed: _loginUser,
+      minWidth: _deviceWidth! * 0.70,
+      height: _deviceHeight! * 0.06,
+      color: Colors.red,
+      child: const Text(
+        'Login',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  Widget _registerPageLink() {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, 'register'),
+      child: const Text(
+        "Don't have an account?",
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 15,
+          fontWeight: FontWeight.w200,
+        ),
+      ),
+    );
+  }
+
+  void _loginUser() {
+    if (_loginFormKey.currentState!.validate()) {
+      _loginFormKey.currentState!.save();
+    }
+  }
+}
---
+++ finstagram/lib/pages/register_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _RegisterPageState();
+  }
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  double? _deviceHeight, _deviceWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth! * 0.05,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _title(),
+              _registerButton(),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
+  Widget _title() {
+    return const Text(
+      'Fintagram',
+      style: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return MaterialButton(
+      onPressed: () {},
+      minWidth: _deviceWidth! * 0.50,
+      height: _deviceHeight! * 0.05,
+      color: Colors.red,
+      child: Text(
+        'Register',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+}
