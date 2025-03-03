b28dde55f4a152742dcaf4bb3babbb8607f2b488
KevinChiputra <kevin.chiputra@binus.ac.id>
2025-03-03 21:26:34 +0700

    feat: Depedency Mocking using Moctail and Dio

--- ibmi/lib/utils/calculator.dart
+++ ibmi/lib/utils/calculator.dart
@@ -1,5 +1,29 @@
+import 'dart:io';
 import 'dart:math';
+
+import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
 
 double calculatorBMI(int height, int weight) {
   return 703 * (weight / pow(height, 2));
+}
+
+Future<double> calculateBMIAsync(Dio _dio) async {
+//   // Create a custom HttpClient that ignores SSL errors
+//   var httpClient = HttpClient()
+//     ..badCertificateCallback =
+//         (X509Certificate cert, String host, int port) => true;
+
+// // Use the custom HttpClient to create a Dio instance
+//   (_dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
+//     return httpClient;
+//   };
+
+  var _result = await _dio.get('https://jsonkeeper.com/b/AKFA');
+  var _data = _result.data;
+  var _bmi = calculatorBMI(
+    _data['Sergio Ramos'][0],
+    _data['Sergio Ramos'][1],
+  );
+  return _bmi;
 }
